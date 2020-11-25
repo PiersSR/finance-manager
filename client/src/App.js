f092982cdfc0a266aad5b24841e1db9e3ad450e9
Piers <piers.rust95@gmail.com>
2020-11-25 13:08:41 +0000

    Income and Expense form functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import config from './data/config.js';
 import Form from "./components/Form";
 import Dropdown from "./components/Dropdown";
 import React, { useEffect, useReg, useState } from 'react';
 import { nanoid } from 'nanoid';
 
-const CATEGORY_MAP = {
-  General: 1
-}
-
-const FREQUENCY_MAP = {
-  Monthly: 1
-}
-
-const CATEGORY_NAMES = Object.keys(CATEGORY_MAP);
-const FREQUENCY_NAMES = Object.keys(FREQUENCY_MAP);
-
-function addIncome(value, categoryid, frequencyid) {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ userID: 1, amount: value, category: CATEGORY_NAMES[categoryid], frequency: FREQUENCY_NAMES[frequencyid] })
-  }
-}
-
 function App(props) {
+  /// @param userID: The current userID
+  /// @param categoryID: The selected category ID
+  /// @param amount: The input amount
+  /// @param frequencyID: The selected frequency
+  /// Creates a POST request to add an income record
+  function addIncome(userID, amount, categoryId, frequencyId) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            inUserID: userID,
+            inAmount: amount,
+            inCategoryID: categoryId,
+            inFrequencyID: frequencyId
+        })
+      }
+
+    fetch(config.fetchURL + "addIncome", requestOptions);
+  }
+
+  /// @param userID: The current userID
+  /// @param categoryID: The selected category ID
+  /// @param amount: The input amount
+  /// @param frequencyID: The selected frequency
+  /// Creates a POST request to add an expense record
+  function addExpense(userID, amount, category, frequency) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        inUserID: userID,
+        inAmount: amount,
+        inCategoryID: category,
+        inFrequencyID: frequency 
+      })
+    }
+  
+    const fetchURL = fetchURL + "addExpense";
+    fetch(fetchURL, requestOptions);
+  }
+  
     return (
     <div className="moneymanager">
       <h1>Money Manager</h1>
+      <h2>Summary</h2>
+      <h2>Income</h2>
         <Form 
+          type="addIncome"
           addIncome={addIncome}
+        />
+      <h2>Expenses</h2>
+        <Form
+          type="addExpense"
+          addExpense={addExpense}
         />
     </div>
   );
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,34 +2,65 @@ import React, { useState } from 'react';
 import Dropdown from './Dropdown';
 
 function Form(props) {
-    const [value, setValue] = useState('');
+    const [amount, setValue] = useState(0.00);
+    const [category, setCategory] = useState("General");
+    const [frequency, setFrequency] = useState("Monthly");
 
+    const CATEGORYID_MAP = {
+        General: 1,
+        Groceries: 3
+    }
+
+    const FREQUENCYID_MAP = {
+        Monthly: 1,
+        Weekly: 2
+    }
+
+    /// Handles any changes made to the amount field
     function handleChange(e) {
         setValue(e.target.value);
     }
 
+    /// Handles the form submission event
     function handleSubmit(e) {
         e.preventDefault();
-        props.addIncome(value);
-        setValue(value);
+
+        switch(props.type) {
+            case "addIncome":
+                props.addIncome(1, amount, CATEGORYID_MAP[category], FREQUENCYID_MAP[frequency]);
+                break;
+            case "addExpense":
+                props.addExpense(1, amount, CATEGORYID_MAP[category], FREQUENCYID_MAP[frequency]);
+                break;
+            default:
+                alert("Error: Submission type was not recognised.");
+        }
+    }
+
+    /// @param e: The element holding the current category value
+    /// Gets the category to the current selected value in the category dropdown
+    function getCategory(inCategory) {
+        setCategory(inCategory);
+    }
+
+    /// @param e: The element holding the current frequency value
+    /// Gets the category to the current selected value in the frequency dropdown
+    function getFrequency(inFrequency) {
+        setFrequency(inFrequency);
     }
 
     return (
         <form onSubmit={handleSubmit}>
-        <h2 className="label-wrapper">
-            <label>
-                Amount:
-            </label>
-        </h2>
+        <label>
+            Amount:
+        </label>
         <input
             type="number"
             name="price"
-            min="0"
+            min="0.00"
             step=".01"
-            id="new-todo-input"
-            className="input input__lg"
             autoComplete="off"
-            value={value}
+            value={amount}
             onChange={handleChange}
         />
         <label>
@@ -37,6 +68,8 @@ function Form(props) {
             <Dropdown
                 getRequest="getCategories"
                 columnName="Category"
+                type="category"
+                getCategory={getCategory}
             />
         </label>
         <label>
@@ -44,9 +77,11 @@ function Form(props) {
             <Dropdown
                 getRequest="getFrequencies"
                 columnName="Frequency"
+                type="frequency"
+                getFrequency={getFrequency}
             />
         </label>
-        <button type="submit" className="btn btn__primary btn__lg">
+        <button type="submit">
             Add
         </button>
         </form>
