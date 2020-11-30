44aca924901c7a2a7d85af7ee0a0fbf8b85cfed8
Piers <piers.rust95@gmail.com>
2020-11-30 16:55:38 +0000

    .

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('Hello! Im in the index file');
   res.render('index', { title: __dirname });
 });
 
